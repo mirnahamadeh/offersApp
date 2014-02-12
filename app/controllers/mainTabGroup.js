52008831b62074f1dc2d8f1902b92e95da7ad26d
mirnahamadeh <gardenia_0809@hotmail.com>
2014-02-12 13:10:25 +0200

    moved rows to js

--- app/controllers/mainTabGroup.js
+++ app/controllers/mainTabGroup.js
@@ -2,3 +2,20 @@ var args = arguments[0] || {};
 function openwin (e) {
   
 }
+
+var rows = [];
+
+for(var i = 0; i< 10; i++){
+	var row = Ti.UI.createTableViewRow({
+		
+	});
+	
+	var v = Ti.UI.createView({
+		
+	});
+	
+	row.add(v);
+	rows.push(row);
+}
+
+$.viewrow1.data = rows;
